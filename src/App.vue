4d5af82fca8017fcaf3ff4ce0b9ad164ef5bc956
darens07 <andresrodrigues634@gmail.com>
2020-12-29 15:59:22 -0400

    MPV alredy

--- src/App.vue
+++ src/App.vue
@@ -4,53 +4,22 @@
       <img alt="Vue logo" src="./assets/logo.png">
       <h1>Welcome to Your Vue.js MPage</h1>
     </div>
-    <div class="mp-data">
-      <div class="mp-object">
-        <div class="mp-pre">
-          <h2>My Filters</h2>
-          <pre>{{ users.filters }}</pre>
-        </div>
-        <div class="mp-pre">
-          <h2>My Orders</h2>
-          <pre>{{ users.orders }}</pre>
-        </div>
-        <div class="mp-pre">
-          <h2>My Page active</h2>
-          <pre>{{ users.page }}</pre>
-        </div>
-        <div class="mp-pre">
-          <h2>My Pages</h2>
-          <pre>{{ users.pages }}</pre>
-        </div>
-        <div class="mp-pre">
-          <h2>My PerPage</h2>
-          <pre>{{ users.perpage }}</pre>
-        </div>
-        <div class="mp-pre">
-          <h2>Status</h2>
-          <pre>{{ users.status }}</pre>
-        </div>
-      </div>
-      <div class="mp-object">
-        <div class="mp-pre">
-          <h2>My Items</h2>
-          <pre>{{ users.items }}</pre>
-        </div>
-      </div>
-    </div>
+    
+    <pre-code :object="users" />
   </div>
 </template>
 
 <script>
 import MPage from './MPage/MPage.js';
+import preCode from './components/preCode';
 
 export default {
   name: 'App',
   data(){
     return{
-        mp_main: new MPage(this.$store, null, 10, ['id', 'name']),
-        page: 1,
-        filter: null
+      mp_main: new MPage(this.$store, null, 10, ['id', 'name']),
+      page: 1,
+      filter: null
     }
   },
   computed:{
@@ -63,7 +32,7 @@ export default {
     }
   },
   components: {
-    //
+    preCode
   },
   methods:{
     //
@@ -80,28 +49,5 @@ export default {
 }
 .mp-introduction{
   text-align: center;
-}
-.mp-data{
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-.mp-object{
-  flex: 0 0 50%;
-  width: 50%;
-  margin: 0px;
-}
-.mp-pre{
-  background: #dedcdc;
-  border-radius: 5px;
-  padding: 1px 20px;
-  margin: 10px 5px;
-}
-
-@media(max-width: 767px){
-  .mp-object{
-    flex: 0 0 100%;
-    width: 100%;
-  }
 }
 </style>
